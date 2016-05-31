e7d32157d03068c8ba9fd7fb2bb50174bc88b80d
Brant Wynn <brant.wynn@acquia.com>
2016-05-31 14:54:09 -0500

    Initial commit.

---
+++ src/Form/ReplicateNodeForm.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\replicate_node_block\Form\ReplicateNodeForm.
+ */
+
+namespace Drupal\replicate_node_block\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+use Drupal\replicate\Replicator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * Class ReplicateNodeForm.
+ *
+ * @package Drupal\replicate_node_block\Form
+ */
+class ReplicateNodeForm extends FormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The event dispatcher.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $eventDispatcher;
+
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, EventDispatcherInterface $eventDispatcher) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->eventDispatcher = $eventDispatcher;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('event_dispatcher')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'replicate_node_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $node = NULL) {
+    $form = [];
+    if ($node != NULL && $nid = $node->id()) {
+      $form['node_id'] = array(
+        '#type' => 'hidden',
+        '#value' => $nid
+      );
+      $form['replicate_node'] = array(
+        '#type' => 'submit',
+        '#value' => $this->t('Replicate Node'),
+      );
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $nid = $form_state->getValue('node_id');
+    $node = Node::load($nid);
+    $replicator = new Replicator($this->entityTypeManager, $this->eventDispatcher);
+    // Do Androids Dream of Electric Sheep?
+    if ($replicant = $replicator->replicateByEntityId('node', $nid)) {
+      $message = $this->t('Node:@nodeId "@nodeTitle" has been replicated as Node:@replicantId "@replicantTitle" and you have been redirected.', [
+        '@nodeId' => $node->id(),
+        '@nodeTitle' => $node->getTitle(),
+        '@replicantId' => $replicant->id(),
+        '@replicantTitle' => $replicant->getTitle(),
+      ]);
+      drupal_set_message($message, 'status');
+      $redirectUrl = Url::fromUserInput('/node/' .  $replicant->id());
+      $form_state->setRedirectUrl($redirectUrl);
+    }
+  }
+
+}
---
+++ src/Plugin/Block/ReplicateNodeBlock.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\replicate_node_block\Plugin\Block\ReplicateNodeBlock.
+ */
+
+namespace Drupal\replicate_node_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilder;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'ReplicateNodeBlock' block.
+ *
+ * @Block(
+ *  id = "replicate_node_block",
+ *  admin_label = @Translation("Replicate node block"),
+ *  context = {
+ *    "node" = @ContextDefinition("entity:node", label = @Translation("Node"))
+ *  },
+ *  category = @Translation("Forms")
+ * )
+ *
+ */
+class ReplicateNodeBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilder
+   */
+  protected $formBuilder;
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilder $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $block = [];
+    $node = $this->getContextValue('node');
+    $block = [$this->formBuilder->getForm('\Drupal\replicate_node_block\Form\ReplicateNodeForm', $node)];
+    return $block;
+  }
+
+}
